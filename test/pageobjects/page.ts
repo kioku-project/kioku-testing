fcd136bdd77d7d8e9893594c6b03fea010fcc44c
Julian <66846059+Julian702@users.noreply.github.com>
2023-08-10 17:14:43 +0200

    substituted baseUrl in page.ts
    Co-authored-by: 4KevR <50447092+4KevR@users.noreply.github.com>

--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -1,15 +1,17 @@
-import { browser } from '@wdio/globals'
+import { browser } from "@wdio/globals";
+
+import { config } from "../../wdio.conf.ts";
 
 /**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
+ * main page object containing all methods, selectors and functionality
+ * that is shared across all page objects
+ */
 export default class Page {
     /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
-    */
-    public open (path: string) {
-        return browser.url(`http://localhost:3000/${path}`)
+     * Opens a sub page of the page
+     * @param path path of the sub page (e.g. /path/to/page.html)
+     */
+    public open(path: string) {
+        return browser.url(`${config.baseUrl}/${path}`);
     }
 }
